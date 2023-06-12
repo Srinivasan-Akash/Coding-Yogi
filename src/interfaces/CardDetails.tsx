2ce64fac1ca5959d2fe93d4dbf84f134118179ad
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-06-12 17:57:50 +0530

    ADded interface for props for resuseable components

---
+++ src/interfaces/CardDetails.tsx
@@ -0,0 +1,10 @@
+export default interface CardDetails {
+    children: React.ReactNode;
+    instructor_name: string;
+    instructor_photo: string;
+    difficult_level: string;
+    lessons: number;
+    thumbnail: string;
+    course_name: string;
+    desc: string;
+}
---
+++ src/interfaces/HeadLine.tsx
@@ -0,0 +1,5 @@
+export default interface HeadLine {
+    children: React.ReactNode;
+    page_name: string;
+    desc: string;
+}
